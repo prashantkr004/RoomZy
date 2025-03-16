0715b8dbf30f4f3c16d7b4b97a4a51dfeb14d911
Sachin Kumar <151154300+Sachin2556@users.noreply.github.com>
2025-03-16 10:45:11 +0530

    feat:Created Otp Screen Component (#9)
    * feat:Created Otp Screen Component
    
    * removed unused commented codes and updated css file

--- Frontend/Roomzy/src/components/forgotPassword/ForgotPassword.jsx
+++ Frontend/Roomzy/src/components/forgotPassword/ForgotPassword.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import './ForgotPassword.css';
+import OtpSender from '../OtpSender/OTPSender'; // Import OtpSender component
 
 const ForgotPassword = ({ onClose }) => {
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+    const [showOtp, setShowOtp] = useState(false); // State to show OTP input
 
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
@@ -11,27 +13,33 @@ const ForgotPassword = ({ onClose }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Here you would typically send the email to your backend to request an OTP
         console.log('Requesting OTP for:', email);
         setMessage(`An OTP has been sent to ${email}.`);
+        setShowOtp(true); // Show OTP input
     };
 
     return (
         <div className="forgot-password-container">
-            <h2>Forgot Password</h2>
-            <p>Enter your email to receive an OTP.</p>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    placeholder="Email Address"
-                    value={email}
-                    onChange={handleEmailChange}
-                    required
-                />
-                <button type="submit">Get OTP</button>
-            </form>
-            {message && <p className="success-message">{message}</p>}
-            <button className="close-modal" onClick={onClose}>Back to log in</button>
+            {!showOtp ? ( // Show this part if OTP is not requested yet
+                <>
+                    <h2>Forgot Password</h2>
+                    <p>Enter your email to receive an OTP.</p>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="email"
+                            placeholder="Email Address"
+                            value={email}
+                            onChange={handleEmailChange}
+                            required
+                        />
+                        <button type="submit">Get OTP</button>
+                    </form>
+                    {message && <p className="success-message">{message}</p>}
+                    <button className="close-modal" onClick={onClose}>Back to log in</button>
+                </>
+            ) : (
+                <OtpSender onClose={() => setShowOtp(false)} />// Show OTP input when button is clicked
+            )}
         </div>
     );
 };
